97b7e484205675fb2d23edf982e805c025d361db
Víctor Orozco <vorozco@nabenik.com>
2018-02-22 16:25:40 -0500

    Adding final touches for presentation

--- src/main/java/com/nabenik/demo/rest/MovieEndpoint.java
+++ src/main/java/com/nabenik/demo/rest/MovieEndpoint.java
@@ -5,10 +5,7 @@ import java.util.List;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,6 +17,8 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.metrics.annotation.Metered;
 
 import com.nabenik.demo.controller.MovieDao;
 import com.nabenik.demo.dto.MovieDTO;
@@ -39,13 +38,6 @@ public class MovieEndpoint {
 	@Inject
 	@ConfigProperty(name = "omdbservice.url")
 	String omdbDaemonServiceUrl;
-	
-	
-	@POST
-	public Response create(final Movie movie) {
-		movieService.create(movie);
-		return Response.ok().build();
-	}
 
 	@GET
 	@Path("/{id:[0-9][0-9]*}")
@@ -57,8 +49,10 @@ public class MovieEndpoint {
 		return Response.ok(movie).build();
 	}
 	
+	@Metered
 	@GET
 	@Path("/expanded/{id:[0-9][0-9]*}")
+	@Fallback(fallbackMethod = "findById")
 	public Response findExpandedById(@PathParam("id") final Long id) {
 		Movie movie = movieService.findById(id);
 		if (movie == null) {
@@ -91,23 +85,6 @@ public class MovieEndpoint {
 			@QueryParam("max") final Integer maxResult) {
 		final List<Movie> movies = movieService.listAll(startPosition, maxResult);
 		return movies;
-	}
-
-	@PUT
-	@Path("/{id:[0-9][0-9]*}")
-	public Response update(@PathParam("id") Long id, final Movie movie) {
-		Movie updatedEntity = movieService.update(movie);
-        if (updatedEntity == null) {
-            return Response.status(Status.NOT_FOUND).build();
-        }
-		return Response.noContent().build();
-	}
-
-	@DELETE
-	@Path("/{id:[0-9][0-9]*}")
-	public Response deleteById(@PathParam("id") final Long id) {
-		movieService.deleteById(id);
-        return Response.ok().build();
 	}
 
 }
